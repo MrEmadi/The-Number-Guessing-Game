eb9cd779125fa5769015c02c5ccd417555079c26
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-05-31 01:06:27 -0700

    feat: implement the user input to guess a number with the try-except error management and while loop

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 from art import logo_of_the_app
+from random import randint
 
 if __name__ == '__main__':
-    try:
-        print(logo_of_the_app)
-        print('Welcome to The Number Guessing Game!')
-        print('\nI\'m thinking of a number between 1 and 100')
-        user_level = input("Choose difficulty. Type 'easy' or 'hard': ")
-        levels = {
+    print(logo_of_the_app)
+    print('Welcome to The Number Guessing Game!')
+    print('\nI\'m thinking of a number between 1 and 100')
+    user_level = input("Choose difficulty. Type 'easy' or 'hard': ")
+    levels = {
             'easy': 10,
             'hard': 5
         }
-        while not user_level in levels:
-            user_level = input("Try again! Choose difficulty. Type 'easy' or 'hard': ")
-        chances = levels[user_level]
-        print(f"You have {chances} attempts remaining to guess the number.")
-    except Exception as e:
-        print(f'\nError -> {e}')
+    while not user_level in levels:
+        user_level = input("Try again! Choose difficulty. Type 'easy' or 'hard': ")
+    chances = levels[user_level]
+    random_number_1_to_100 = randint(1, 100) # [1, 100]
+    while chances > 0:
+        try:
+            print(f"You have {chances} attempts remaining to guess the number.")
+            user_guess = int(input('\nMake a guess: '))
+            if not 1 <= user_guess <= 100:
+                raise Exception('Guess must be a number between 1 and 100')
+        except ValueError:
+            print('\nError -> You must enter a number! Try again.\n')
+        except Exception as error:
+            print(f'\nError -> {error}! Try again.\n')
